0bd3a488b8def82cabd8bca02e8e62f13432d52b
priyanshijoshiii <priyanshij2233@gmail.com>
2026-07-05 00:07:10 +0530

    changed the model

--- app/api/analyze/route.ts
+++ app/api/analyze/route.ts
@@ -39,7 +39,7 @@ export async function POST(request: NextRequest) {
 
     // Llama analysis
     const completion = await groq.chat.completions.create({
-      model: 'llama-3.3-70b-specdec',
+      model: 'model: "llama-3.3-70b-versatile',
       temperature: 0.3,
       messages: [
       {
